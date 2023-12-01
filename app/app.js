7ae3aa4082de4212b481bf0a975bfefa35b73ed5
pillow12360 <ehdcks1256@naver.com>
2023-12-02 00:53:10 +0900

    노드몬 적용

--- app/app.js
+++ app/app.js
@@ -4,14 +4,15 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
-
 // 라우팅
 const home = require("./src/routes/home");
 
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/src/public`));
+// 정적 경로 추가
 
 app.use("/", home);
 // use => 미들웨어 등록 메서드
--- app/bin/www.js
+++ app/bin/www.js
@@ -2,7 +2,7 @@
 
 const PORT = 3000;
 
-const app = require("../../app");
+const app = require("../app");
 
 app.listen(PORT, () => {
   console.log("서버 가동");
